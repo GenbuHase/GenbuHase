fa4939c644fac7353fa6b85433b15933a979bb20
Genbu Hase <hasegenbu@gmail.com>
2018-11-18 09:08:31 +0900

    ejsを採用

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 const gulp = require("gulp");
 const gulpPlumber = require("gulp-plumber");
+const gulpRename = require("gulp-rename");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
 const webpackStream = require("webpack-stream");
 const sass = require("gulp-sass");
+const ejs = require("gulp-ejs");
 
 gulp.task("default", ["watch"]);
 
 gulp.task("watch", ["compile"], () => {
-	gulp.watch("src/js/**/*.js", ["js-compile"]);
-	gulp.watch("src/css/**/*.scss", ["scss-compile"]);
+	gulp.watch("./src/js/**/*.js", ["js-compile"]);
+	gulp.watch("./src/css/**/*.scss", ["scss-compile"]);
+	gulp.watch("./src/**/*.ejs", ["ejs-compile"]);
 });
 
-gulp.task("compile", ["js-compile", "scss-compile"]);
+gulp.task("compile", ["js-compile", "scss-compile", "ejs-compile"]);
 
 gulp.task("js-compile", () => {
 	webpackStream(webpackConfig, webpack)
@@ -21,12 +24,15 @@ gulp.task("js-compile", () => {
 });
 
 gulp.task("scss-compile", () => {
-	gulp.src("src/css/**/*.scss")
+	gulp.src("./src/css/**/*.scss")
 		.pipe(gulpPlumber())
-		.pipe(sass({ sourceMap: true }))
+		.pipe(sass({ outputStyle: "expanded", sourceMap: true }))
 		.pipe(gulp.dest("./src/css"));
 });
 
-gulp.task("html-compile", () => {
-	
+gulp.task("ejs-compile", () => {
+	gulp.src(["./src/**/*.ejs", "!./src/**/_*.ejs"])
+		.pipe(ejs())
+		.pipe(gulpRename({ extname: ".html" }))
+		.pipe(gulp.dest("."));
 });
