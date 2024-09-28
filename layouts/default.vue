f80b7033151ce08c28c31456413edcbb940fe19b
どっかのげんちゃん。 <14960841+GenbuHase@users.noreply.github.com>
2024-09-29 05:03:38 +0900

    0.1.0.4 + モバイルメニューを実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
         </div>
 
         <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5" @click="openMenu">
             <span class="sr-only">Open main menu</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -24,17 +24,15 @@
       </nav>
 
       <!-- Mobile menu, show/hide based on menu open state. -->
-      <div class="hidden lg:hidden" role="dialog" aria-modal="true">
+      <div ref="menu" class="menu lg:hidden" role="dialog" aria-modal="true" v-if="Menu_isOpened">
         <!-- Background backdrop, show/hide based on slide-over state. -->
         <div class="fixed inset-0 z-50"></div>
 
         <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
-              <NuxtLink to="/" class="brand-name -m-1.5 p-1.5 text-gray-900">Genbu Hase</NuxtLink>
-            </a>
+            <div />
 
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="closeMenu">
               <span class="sr-only">Close menu</span>
 
               <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -83,3 +81,36 @@
     </footer>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .menu {
+    @apply transition;
+    @apply duration-200;
+    @apply ease-out;
+    @apply opacity-0;
+
+    &.open {
+      @apply transition;
+      @apply duration-150;
+      @apply ease-in; 
+      @apply opacity-100;
+    }
+  }
+</style>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const menu = ref();
+  const Menu_isOpened = ref(false);
+
+  function openMenu () {
+    Menu_isOpened.value = true;
+    setTimeout(() => menu.value.classList.add("open"));
+  }
+
+  function closeMenu () {
+    menu.value.classList.remove("open");
+    setTimeout(() => Menu_isOpened.value = false, 150);
+  }
+</script>
