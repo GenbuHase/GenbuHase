093a6879f1797a0fe1577005b63d95a726b6199a
GenbuHase <GenbuHase@users.noreply.github.com>
2024-10-05 00:16:12 +0900

    deploy: 59fed08282c8f831c03f531e3a77fee22ea4d64b

--- _nuxt/Dvejx17R.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./C747WaUr.js";import{_ as s}from"./DlAUqK2U.js";import{c,b as a,a as r,q as n,o as _}from"./Ccn345Vz.js";import"./BRZ-mF04.js";const m={},f={class:"text-base-content bg-[#f4b3c2]"};function d(e,i){const o=t;return _(),c("section",f,[a(o),r("div",null,[n(e.$slots,"default")])])}const h=s(m,[["render",d]]);export{h as default};
