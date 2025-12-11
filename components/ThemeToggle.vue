e3f23256ca5a78843b23381836f50042a13746e7
長谷 玄武 (Genbu Hase) <14960841+GenbuHase@users.noreply.github.com>
2025-12-12 02:13:58 +0900

    1.1.1 + カラーモード切り替え時にアニメーションする機能を実装

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,9 +1,30 @@
 <template>
   <button class="btn btn-ghost btn-circle" @click="toggleTheme" aria-label="Toggle Theme">
-    <Icon v-if="isDark" name="heroicons:moon-20-solid" class="w-6 h-6" />
-    <Icon v-else name="heroicons:sun-20-solid" class="w-6 h-6" />
+    <ClientOnly>
+      <Transition name="rotate" mode="out-in">
+        <Icon v-if="isDark" name="heroicons:moon-20-solid" class="w-6 h-6" />
+        <Icon v-else name="heroicons:sun-20-solid" class="w-6 h-6" />
+      </Transition>
+    </ClientOnly>
   </button>
 </template>
+
+<style scoped>
+  .rotate-enter-active,
+  .rotate-leave-active {
+    transition: all 0.25s ease-in-out;
+  }
+
+  .rotate-enter-from {
+    opacity: 0;
+    transform: rotate(-90deg) scale(0.5);
+  }
+
+  .rotate-leave-to {
+    opacity: 0;
+    transform: rotate(90deg) scale(0.5);
+  }
+</style>
 
 <script setup lang="ts">
   const colorMode = useColorMode();
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="text-base-content bg-[#f4b3c2] dark:bg-base-300">
+  <section class="text-base-content bg-[#f4b3c2] dark:bg-base-300 transition-colors duration-500">
     <Header />
 
     <!-- https://github.com/nuxt/nuxt/issues/24632 -->
